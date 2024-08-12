b2d02c0c54ad1bb2b9e2d7de80b9a24023399ec7
avbdasf <simlee@seas.upenn.edu>
2024-08-12 17:47:50 +0800

    updated argparse

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ from data_processing import *
 import argparse
 
 def main():
-    """
     # Argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("--folderPath", help="Path to the folder containing the PDF files for one month")
     args = parser.parse_args()
-    """
 
-    process_folder("data/factsheets/2024/June")
-    #process_folder_json("factsheets/json/2024/April")
-    #print(get_return_from_pdf("data/factsheets/2024/June/Akahi Fund LP June 2024 Newsletter.pdf"))
+    process_folder(args.folderPath)
 
 if __name__ == "__main__":
     main()
