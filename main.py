8219abd331ecc3403e11e2eaac156f54bf6dd971
avbdasf <simlee@seas.upenn.edu>
2024-08-13 16:10:19 +0800

    fix directory

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from database import *
 from data_processing import *
 import argparse
+import os
 
 def main():
     # Argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("--folderPath", help="Path to the folder containing the PDF files for one month")
     args = parser.parse_args()
+    cwd = os.getcwd()
 
-    process_folder(args.folderPath)
+    process_folder(os.path.join(cwd, args.folderPath))
 
 if __name__ == "__main__":
     main()
